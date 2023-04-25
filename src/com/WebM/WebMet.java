dbfd2ed4ee41e4bd13a53be347fac747333f2595
peerjas22 <peermohamed8492@gmail.com>
2023-04-25 20:33:35 +0530

    my working java project updated

---
+++ src/com/WebM/WebMet.java
@@ -0,0 +1,15 @@
+package com.WebM;
+
+import org.openqa.selenium.WebDriver;
+
+public class WebMet {
+	
+	public static void main(String[] args) {
+		System.setProperty("Webdriver.Chrome.driver",
+				"C:\\Users\\peerjasmine\\eclipse-workspace\\My  Workings\\DrivCh\\chromedriver.exe");
+		WebDriver web = new WebDriver();
+		
+		
+	}
+
+}
